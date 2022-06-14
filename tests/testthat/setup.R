38cf7dbba80ff0e9945b681ff3d796703734aba0
Jacob Clarke <43381660+jacobaclarke@users.noreply.github.com>
2022-06-13 20:18:16 -0600

    removed with,r fs, and dplyr from dependencies

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,4 @@
 library(fs)
-library(dplyr)
 dir_fixture <- function(dir = file_temp(), env = parent.frame()) {
   op <- options(tidyiddr_cache_dir = dir)
   withr::defer(options(op), envir = env)
--- tests/testthat/test_cache_download.R
+++ tests/testthat/test_cache_download.R
@@ -5,19 +5,19 @@ test_that("caches to memory and disk by default", {
   dir <- dir_fixture()
   cache_download(url)
   memory_name <- (paste0(janitor::make_clean_names(url), ".memory"))
-  memory_name %>%
-    exists() %>%
+  memory_name |>
+    exists() |>
     expect_true()
-  dir %>%
-    list.files() %>%
+  dir |>
+    list.files() |>
     expect_length(1)
 })
 
 test_that("can turn of cache to disk with params", {
   dir <- dir_fixture()
   cache_download(url, use_disk = FALSE)
-  dir %>%
-    list.files() %>%
+  dir |>
+    list.files() |>
     expect_length(0)
 })
 
@@ -26,8 +26,8 @@ test_that("can turn off cache to disk with options", {
   op <- options(tidyiddr_use_cache = F)
   withr::defer(options(op))
   cache_download(url)
-  dir %>%
-    list.files() %>%
+  dir |>
+    list.files() |>
     expect_length(0)
 })
 
@@ -37,14 +37,14 @@ test_that("cache busting works", {
   a <- data.frame(a = 1, b = 3, c = 5)
   write.csv(a, url, row.names = FALSE)
   aread <- cache_download(url)
-  aread %>%
-    as.data.frame() %>%
+  aread |>
+    as.data.frame() |>
     expect_equal(a)
   b <- data.frame(b = 2, a = 1, c = 3)
   write.csv(b, url, row.names = FALSE)
   bread <- cache_download(url, bust_cache = T)
 
-  bread %>%
-    as.data.frame() %>%
+  bread |>
+    as.data.frame() |>
     expect_equal(b)
 })
